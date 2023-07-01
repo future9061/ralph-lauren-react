2e4238073200e55abcde8abe819ed365a174da57
future9061 <132829711+future9061@users.noreply.github.com>
2023-07-01 17:04:24 +0900

    Add files via upload

--- src/components/context/AuthContext.jsx
+++ src/components/context/AuthContext.jsx
@@ -18,8 +18,10 @@ export function AuthContextProvider({ children }) {
   }, []);
 
   //2. context의 data를 보낼 컴포넌트들에게 context의 변화를 알림(value가 바뀌면 렌더링)
-  //나는 로그인 말고 현재 로그인한 유저의 정보만 가져와서 관리자 uid랑 비교할거라
-  //파라미터로 user만 넣으면 됨
+  //property: function 형식으로 가져옴
+  //객체 리터럴에서 함수를 정의할 때는 { propertyName: function() { ... } }과 같은 형식으로 작성
+  //왜 객체 리터럴로 함수를 가져와야 하지?: 비구조분해할당으로 함수를 가져올 때는 그렇게 해야하는 듯
+  //destructuring Assignment
   return (
     <AuthContext.Provider value={{ user, login: login, logout: logout }}>
       {children} {/* 여기에 context 구독할 컴포넌트 넣을거니까.. app.js*/}
